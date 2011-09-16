c792447323afa161271f8353f87338fbb3944133
Qiang Zhou <qiang@gsyc.es>
2011-09-16 16:49:18 +0200

    RegisterTest
    Change-Id: Icfa05953fb7d937775fbce7739419432a46931fd

--- test/com/kurento/commos/sip/RegisterTest.java
+++ test/com/kurento/commos/sip/RegisterTest.java
@@ -10,70 +10,92 @@ import com.kurento.commons.sip.SipEndPointListener;
 import com.kurento.commons.sip.UA;
 import com.kurento.commons.sip.agent.UaFactory;
 import com.kurento.commons.sip.event.SipEndPointEvent;
+import com.kurento.commons.sip.event.SipEventType;
 import com.kurento.commons.sip.util.SipConfig;
 import com.kurento.commos.utils.SipEndPointController;
 
 public class RegisterTest extends TestCase {
 	
 	private final static Log log = LogFactory.getLog(RegisterTest.class);
-	private final static String HOST_37 = "193.147.51.37";
-	private final static String HOST2 = "193.147.51.17";
+	
+	
 	private final static String LINPHONE_HOST = "sip.linphone.org";
-	private final static String LINPHONE_USER = "user1";
-	private final static String LINPHONE_PASS = "pass1";
+	private final static String LINPHONE_USER = "user-test";
+	private final static String LINPHONE_PASS = "password";
 	private final static int LINPHONE_PORT = 5060;
 	private final static String LOCAL_IP= "193.147.51.20";
 	
 	private final static int WAIT_TIME = 100;
-//	private final static String HOST = LOCAL_IP;
-	private final static int proxyPort= 5080;
 	private final static int localPort= 5040;
-	
-//	boolean isRegister;
-//	SipEndPoint endpoint;
-	
+	SipConfig config;
+	UA userAgent ;
+
 	public RegisterTest(){
 		org.apache.log4j.BasicConfigurator.configure();
 	}
 	
 	@Override
 	protected void setUp() throws Exception {
-
+		config = new SipConfig();
+		config.setProxyAddress(LINPHONE_HOST);
+		config.setProxyPort(LINPHONE_PORT);
+		config.setLocalAddress(LOCAL_IP);
+		config.setLocalPort(localPort);
+		userAgent = UaFactory.getInstance(config);
 	}
 	
 	@Override
 	protected void tearDown() throws Exception {
-		
+		userAgent.terminate();
 	}
 	
 	
 	
 	public void testCRegister() throws Exception {
 		
-		log.info("--------------------------------------------------------------");
-		SipConfig config = new SipConfig();
-		config.setProxyAddress(HOST_37);
-		config.setProxyPort(proxyPort);
-		config.setLocalAddress(LOCAL_IP);
-		config.setLocalPort(localPort);
-		UA userAgent = UaFactory.getInstance(config);
+		log.info("-----------------------------Test for register fail---------------------------------");
+		
+		
 		log.info("User agent initialize with config<< "+ config.toString()+">>");
 		SipEndPointController registerController =  new SipEndPointController("Resgister listener");
-		SipEndPoint endpoint = userAgent.registerEndPoint("quizh", "urjc.es", "linphone123", 3000, registerController);
+		SipEndPoint endpoint = userAgent.registerEndPoint(LINPHONE_USER, LINPHONE_HOST, "none", 3000, registerController);
+		SipEndPointEvent event = registerController.pollSipEndPointEvent(WAIT_TIME);
+		assertEquals(SipEndPointEvent.REGISTER_USER_FAIL, event.getEventType());
+		log.info("Register user fail, OK");
+		log.info("-------------------------------Test finished-----------------------------------------");
 		
-		 SipEndPointEvent event = registerController.pollSipEndPointEvent(WAIT_TIME);
+	}
+	
+	public void testRegisterSuccesfull() throws Exception {
 		
-		 log.info("Event type is "+event.getEventType());
-
-		userAgent.terminate();
-
-		
-		//assertEquals(true, isRegister);
-		log.info("Test finished");
-		
-		//call.cancel();
+		log.info("-----------------------------Test for register Successfull---------------------------------");
+		log.info("User agent initialize with config<< "+ config.toString()+">>");
+		SipEndPointController registerController =  new SipEndPointController("Resgister listener");
+		SipEndPoint endpoint = userAgent.registerEndPoint(LINPHONE_USER, LINPHONE_HOST, LINPHONE_PASS, 3000, registerController);
+		SipEndPointEvent event = registerController.pollSipEndPointEvent(WAIT_TIME);
+		assertEquals(SipEndPointEvent.REGISTER_USER_SUCESSFUL, event.getEventType());
+		log.info("Register user Succesful, OK");
+		log.info("-------------------------------Test finished-----------------------------------------");
 		
 	}
 
+	public void testRegisterNotAuth() throws Exception {
+		
+		log.info("-----------------------------Test for register without authetication---------------------------------");
+		config = new SipConfig();
+		config.setProxyAddress("193.147.51.37");
+		config.setProxyPort(5080);
+		config.setLocalAddress(LOCAL_IP);
+		config.setLocalPort(5060);
+		userAgent = UaFactory.getInstance(config);
+		log.info("User agent initialize with config<< "+ config.toString()+">>");
+		SipEndPointController registerController =  new SipEndPointController("Resgister listener");
+		SipEndPoint endpoint = userAgent.registerEndPoint(LINPHONE_USER, LINPHONE_HOST, LINPHONE_PASS, 3000, registerController);
+		SipEndPointEvent event = registerController.pollSipEndPointEvent(WAIT_TIME);
+		assertEquals(SipEndPointEvent.REGISTER_USER_SUCESSFUL, event.getEventType());
+		log.info("Register user Succesful, OK");
+		log.info("-------------------------------Test finished-----------------------------------------");
+		
+	}
 
 }
