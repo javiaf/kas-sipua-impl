3386707c2adf96d2f283f899adfe54905d118ebb
Qiang Zhou <qiang@gsyc.es>
2011-09-27 11:25:31 +0200

    Reject Test
    Change-Id: Ic29efc4948155ff4baa681cfa7b2db6ee5ba3d0a

--- test/com/kurento/commos/sip/InviteTest.java
+++ test/com/kurento/commos/sip/InviteTest.java
@@ -34,12 +34,16 @@ public class InviteTest extends TestCase {
 	private final int  SERVLET_PORT = 5080; 
 	private final static int WAIT_TIME = 100;
 	private final static int localPort= 5040;
-	SipConfig config;
+	SipConfig config;	
+	private static boolean initalized;
 	UA userAgent1 ;
 	UA userAgent2 ;
 
 	public InviteTest(){
-		org.apache.log4j.BasicConfigurator.configure();
+		if (!initalized) {
+			initalized = true;
+			org.apache.log4j.BasicConfigurator.configure();
+		}
 	}
 	
 	@Override
@@ -140,11 +144,56 @@ public void testSetupAndDropFromCalled() throws Exception {
 	assertEquals(SipCallEvent.CALL_TERMINATE, callSetup40.getEventType());
 
 	SipCallEvent call30SetupEvent = call30Listener.pollSipEndPointEvent(WAIT_TIME);
-	assertEquals(SipCallEvent.CALL_CANCEL, call30SetupEvent.getEventType());
+	assertEquals(SipCallEvent.CALL_SETUP, call30SetupEvent.getEventType());
 	
 	call30SetupEvent = call30Listener.pollSipEndPointEvent(WAIT_TIME);
 	assertEquals(SipCallEvent.CALL_TERMINATE, call30SetupEvent.getEventType());
 	Thread.sleep(200000);
+	
+	log.info("-------------------------------Test finished-----------------------------------------");
+
+}
+
+
+public void testReject() throws Exception {
+	
+	log.info("-----------------------------Test for test setup and drop from calling party---------------------------------");
+	log.info("User agent initialize with config<< "+ config.toString()+">>");
+	SipEndPointController registerAController =  new SipEndPointController("Resgister listener");
+	
+	String user40Name = "user40";
+	SipEndPoint endpoint40 = userAgent1.registerEndPoint(LINPHONE_USER, PROXYL_IP, LINPHONE_PASS, 3600, registerAController);
+	SipEndPointEvent event40 = registerAController.pollSipEndPointEvent(WAIT_TIME);
+	assertEquals(SipEndPointEvent.REGISTER_USER_SUCESSFUL, event40.getEventType());
+	
+	SipEndPointController register30Controller =  new SipEndPointController("Resgister listener");
+	String user30 = "sip:quizh@"+PROXYL_IP+":"+LINPHONE_PORT;
+	SipEndPoint endpoint30 = userAgent2.registerEndPoint("quizh", PROXYL_IP, "linphone123", 3600, register30Controller);
+	SipEndPointEvent event30 = register30Controller.pollSipEndPointEvent(WAIT_TIME);
+	assertEquals(SipEndPointEvent.REGISTER_USER_SUCESSFUL, event30.getEventType());
+	
+	SipCallController callListener40 = new SipCallController();
+	SipCall initialCall40 = endpoint40.dial(user30, callListener40);
+	
+	SipEndPointEvent incomingCall30Event = register30Controller.pollSipEndPointEvent(WAIT_TIME+20000);
+	assertEquals(SipEndPointEvent.INCOMING_CALL, incomingCall30Event.getEventType());
+	SipCall sipcall30 = incomingCall30Event.getCallSource();
+	SipCallController call30Listener = new SipCallController();
+	sipcall30.addListener(call30Listener);
+
+	//initialCall40.cancel();
+	sipcall30.reject();
+			
+	
+	SipCallEvent callSetup40 = callListener40.pollSipEndPointEvent(WAIT_TIME);
+	assertEquals(SipCallEvent.CALL_REJECT, callSetup40.getEventType());
+
+	SipCallEvent call30SetupEvent = call30Listener.pollSipEndPointEvent(WAIT_TIME);
+//	assertEquals(SipCallEvent.CALL_CANCEL, call30SetupEvent.getEventType());
+	
+//	call30SetupEvent = call30Listener.pollSipEndPointEvent(WAIT_TIME);
+//	assertEquals(SipCallEvent.CALL_TERMINATE, call30SetupEvent.getEventType());
+
 	
 	log.info("-------------------------------Test finished-----------------------------------------");
 
