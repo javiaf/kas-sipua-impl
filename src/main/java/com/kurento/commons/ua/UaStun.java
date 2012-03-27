fe4dedd308490b7100cce74e92259e2b65212fc8
Raúl Benítez <rbenitez@gsyc.es>
2012-03-27 10:03:04 +0200

    Add a new interface that extends UA interface

---
+++ src/main/java/com/kurento/commons/ua/UaStun.java
@@ -0,0 +1,12 @@
+package com.kurento.commons.ua;
+
+import de.javawi.jstun.test.DiscoveryInfo;
+
+public interface UaStun extends UA {
+
+	/**
+	 * Return the information of the Stun
+	 */
+	public DiscoveryInfo getConnectionType();
+
+}
