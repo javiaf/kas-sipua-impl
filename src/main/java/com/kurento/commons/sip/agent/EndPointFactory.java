cc7129bdc7ad6005be2091754c76c3a3964e5d45
Javier Lopez <fjlopez@gsyc.es>
2012-05-07 18:19:23 +0200

    Refactor EndPoint factory to add scheduler

--- src/main/java/com/kurento/commons/sip/agent/EndPointFactory.java
+++ src/main/java/com/kurento/commons/sip/agent/EndPointFactory.java
@@ -7,9 +7,21 @@ import android.content.Context;
 import com.kurento.commons.ua.EndPoint;
 import com.kurento.commons.ua.EndPointListener;
 import com.kurento.commons.ua.UA;
+import com.kurento.commons.ua.timer.KurentoUaTimer;
 
 public class EndPointFactory {
 
+	public static EndPoint getInstance(String userName, String realm,
+			String password, int expires, UA ua, EndPointListener handler,
+			KurentoUaTimer timer) throws Exception {
+		if (!(ua instanceof UaImpl))
+			throw new SipException("Incorrect UA");
+		EndPoint endPoint = new SipEndPointImpl(userName, realm, password,
+				expires, (UaImpl) ua, handler, timer);
+		return endPoint;
+	}
+
+	@Deprecated
 	public static EndPoint getInstance(String userName, String realm,
 			String password, int expires, UA ua, EndPointListener handler,
 			Context context) throws Exception {
