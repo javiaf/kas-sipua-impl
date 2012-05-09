55704c4a756600e1c258bd284217f3f5fee59114
Raúl Benítez <rbenitez@gsyc.es>
2012-05-09 10:52:26 +0200

    Implement initTest

--- src/test/java/com/kurento/commons/sip/junit/RejectTest.java
+++ src/test/java/com/kurento/commons/sip/junit/RejectTest.java
@@ -1,10 +1,16 @@
 package com.kurento.commons.sip.junit;
 
+import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.kurento.commons.sip.agent.EndPointFactory;
+import com.kurento.commons.sip.agent.UaFactory;
+import com.kurento.commons.sip.testutils.MediaSessionDummy;
 import com.kurento.commons.sip.testutils.SipEndPointController;
+import com.kurento.commons.sip.testutils.TestConfig;
 import com.kurento.commons.sip.testutils.TestTimer;
+import com.kurento.commons.sip.util.SipConfig;
 import com.kurento.commons.ua.EndPoint;
 import com.kurento.commons.ua.UA;
 
@@ -26,8 +32,53 @@ public class RejectTest {
 	private static String clientName = "client";
 	private static String serverUri = "sip:" + serverName + "@" + domain;
 	private static String clientUri = "sip:" + clientName + "@" + domain;
+	private static int expires = 1000;
 
 	private static EndPoint serverEndPoint;
 	private static EndPoint clientEndPoint;
+
+	@BeforeClass
+	public static void initTest() throws Exception {
+
+		log.info("Initialice SIP UA to RejectTest");
+
+		UaFactory.setMediaSession(new MediaSessionDummy());
+
+		SipConfig cConfig = new SipConfig();
+		cConfig.setProxyAddress(TestConfig.PROXY_IP);
+		cConfig.setProxyPort(TestConfig.PROXY_PORT);
+		cConfig.setLocalPort(TestConfig.CLIENT_PORT);
+		cConfig.setLocalAddress("lo0");
+
+		serverUa = UaFactory.getInstance(cConfig);
+		serverEndPointController = new SipEndPointController(serverName);
+		serverTimer = new TestTimer();
+		// Create and register SIP EndPoint
+		serverEndPoint = EndPointFactory.getInstance(serverName, "kurento.com",
+				"", expires, serverUa, serverEndPointController, serverTimer,
+				false);
+		// Create SIP stack and activate SIP EndPoints
+		serverUa.reconfigure();
+
+		log.info("	ServerUa created");
+
+		SipConfig sConfig = new SipConfig();
+		sConfig.setProxyAddress(TestConfig.CLIENT_IP);
+		sConfig.setProxyPort(TestConfig.CLIENT_PORT);
+		sConfig.setLocalPort(TestConfig.PROXY_PORT);
+		sConfig.setLocalAddress("lo0");
+
+		clientUa = UaFactory.getInstance(sConfig);
+		clientEndPointController = new SipEndPointController("client");
+		clientTimer = new TestTimer();
+		clientEndPoint = EndPointFactory.getInstance(clientName, "kurento.com",
+				"", expires, clientUa, clientEndPointController, clientTimer,
+				false);
+		// Create SIP stack and activate SIP EndPoints
+		clientUa.reconfigure();
+
+		log.info("	ClientUa created");
+
+	}
 
 }
