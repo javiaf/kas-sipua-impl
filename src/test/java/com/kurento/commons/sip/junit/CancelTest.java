ad3a37e105c9003b3e128354bf4bb3cc5f5a5e41
Raúl Benítez <rbenitez@gsyc.es>
2012-05-09 12:16:51 +0200

    Add initTest of the CancelTest

--- src/test/java/com/kurento/commons/sip/junit/CancelTest.java
+++ src/test/java/com/kurento/commons/sip/junit/CancelTest.java
@@ -16,11 +16,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 package com.kurento.commons.sip.junit;
 
+import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.kurento.commons.sip.agent.EndPointFactory;
+import com.kurento.commons.sip.agent.UaFactory;
+import com.kurento.commons.sip.testutils.MediaSessionDummy;
 import com.kurento.commons.sip.testutils.SipEndPointController;
+import com.kurento.commons.sip.testutils.TestConfig;
 import com.kurento.commons.sip.testutils.TestTimer;
+import com.kurento.commons.sip.util.SipConfig;
 import com.kurento.commons.ua.EndPoint;
 import com.kurento.commons.ua.UA;
 
@@ -50,5 +56,45 @@ public class CancelTest {
 
 	private static EndPoint serverEndPoint;
 	private static EndPoint clientEndPoint;
+
+
+	@BeforeClass
+	public static void initTest() throws Exception {
+
+		log.info("Initialice SIP UA Cancel test");
+
+		UaFactory.setMediaSession(new MediaSessionDummy());
+
+		SipConfig cConfig = new SipConfig();
+		cConfig.setProxyAddress(TestConfig.PROXY_IP);
+		cConfig.setProxyPort(TestConfig.PROXY_PORT);
+		cConfig.setLocalPort(TestConfig.CLIENT_PORT);
+		cConfig.setLocalAddress("lo0");
+
+		serverUa = UaFactory.getInstance(cConfig);
+		serverEndPointController = new SipEndPointController(serverName);
+		serverTimer = new TestTimer();
+		// Create and register SIP EndPoint
+		serverEndPoint = EndPointFactory.getInstance(serverName, "kurento.com",
+				"", expires, serverUa, serverEndPointController, serverTimer,
+				false);
+		// Create SIP stack and activate SIP EndPoints
+		serverUa.reconfigure();
+
+		SipConfig sConfig = new SipConfig();
+		sConfig.setProxyAddress(TestConfig.CLIENT_IP);
+		sConfig.setProxyPort(TestConfig.CLIENT_PORT);
+		sConfig.setLocalPort(TestConfig.PROXY_PORT);
+		sConfig.setLocalAddress("lo0");
+
+		clientUa = UaFactory.getInstance(sConfig);
+		clientEndPointController = new SipEndPointController("client");
+		clientTimer = new TestTimer();
+		clientEndPoint = EndPointFactory.getInstance(clientName, "kurento.com",
+				"", 10, clientUa, clientEndPointController, clientTimer, false);
+		// Create SIP stack and activate SIP EndPoints
+		clientUa.reconfigure();
+
+	}
 
 }
