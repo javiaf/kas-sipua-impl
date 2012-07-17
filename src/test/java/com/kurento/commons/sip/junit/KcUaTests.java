8f395e52a8ae6eea02461092b40ff5eb28ef9625
Miguel París Díaz <mparisdiaz@gmail.com>
2012-07-17 17:13:53 +0200

    Add kc-ua tests.

---
+++ src/test/java/com/kurento/commons/sip/junit/KcUaTests.java
@@ -0,0 +1,12 @@
+package com.kurento.commons.sip.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.kurento.ua.commons.junit.ProbeTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ProbeTest.class })
+public class KcUaTests {
+
+}
